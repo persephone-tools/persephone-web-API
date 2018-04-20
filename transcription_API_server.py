bfa5161ae5d7186482e9b16d5c8d637b00cd39ec
Janis <janis@jaggedverge.com>
2018-04-20 18:34:10 +1000

    Use format keyword argument

--- transcription_API_server.py
+++ transcription_API_server.py
@@ -10,9 +10,9 @@ app.add_api('api_spec.yaml', resolver=RestyResolver('api'))
 @app.route('/')
 def index():
     return """Access to the API is via the API versioned path prefix
-<a href="/{0}">/{0}</a>. The API explorer tool can be found at
-<a href="/{0}/ui/">/{0}/ui/</a>, this is the best place to explore the API.
-""".format("v0.1")
+<a href="/{version}">/{version}</a>. The API explorer tool can be found at
+<a href="/{version}/ui/">/{version}/ui/</a>, this is the best place to explore the API.
+""".format(version="v0.1")
 
 app.run(port=8080)
 
