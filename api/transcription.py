fac4578373d221472ac638d7c878c44a9a8bec4a
Janis <janis@jaggedverge.com>
2018-04-20 20:49:19 +1000

    Stub GET for transcription endpoint

---
+++ api/transcription.py
@@ -0,0 +1,8 @@
+"""
+API endpoints for /transcription
+This deals with the API access for transcription files uploading/downloading.
+"""
+
+def get(transcriptionID):
+    print("Got transcription file ID {}".format(transcription))
+    return "Get transcription info not implemented", 501
