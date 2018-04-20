154ae35e0ea9fe198237bb67f593b43bd9fd155d
Janis <janis@jaggedverge.com>
2018-04-20 18:20:07 +1000

    Added root path

--- transcription_API_server.py
+++ transcription_API_server.py
@@ -5,5 +5,14 @@ import api
 
 app = connexion.FlaskApp(__name__, specification_dir='swagger/')
 app.add_api('api_spec.yaml', resolver=RestyResolver('api'))
+
+
+@app.route('/')
+def index():
+    return """Access to the API is via the API versioned path prefix
+<a href="/{0}">/{0}</a>. The API explorer tool can be found at
+<a href="/{0}/ui/">/{0}/ui/</a>, this is the best place to explore the API.
+""".format("v0.1")
+
 app.run(port=8080)
 
