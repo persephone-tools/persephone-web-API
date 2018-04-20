84daf0cc3697b8aa6753bf81ff185780cbdd6b8e
Janis <janis@jaggedverge.com>
2018-04-20 20:59:09 +1000

    Stub POST for transcription endpoint

--- api/transcription.py
+++ api/transcription.py
@@ -3,6 +3,11 @@ API endpoints for /transcription
 This deals with the API access for transcription files uploading/downloading.
 """
 
+def post(transcriptionFile):
+    """handle POST request for transcription file"""
+    print("Got {}".format(transcriptionFile))
+    return "transcription upload not implemented", 501
+
 def get(transcriptionID):
     print("Got transcription file ID {}".format(transcription))
     return "Get transcription info not implemented", 501
