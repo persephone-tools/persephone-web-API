8f3ad91bea6a92cc71f8e60f4f4e73a30c0ba1f9
Janis <janis@jaggedverge.com>
2018-04-20 20:32:04 +1000

    Stub GET method for audio file information

--- api/audio.py
+++ api/audio.py
@@ -7,3 +7,13 @@ def post(audioFile):
     """handle POST request for audio file"""
     print("Got {}".format(audioFile))
     return "Audio upload not implemented", 501
+
+
+def get(audioID):
+    """Handle GET request for audio file information.
+    Note that this does not return the audio file directly but
+    rather a JSON object with the relevant information.
+    This allows the flexibility of file storage being handled
+    by another service that is outside this API service."""
+    print("Got audio file ID {}".format(audioID))
+    return "Get audio info not implemented", 501
