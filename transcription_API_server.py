19c9b91a385641300ce74544868dfffd9af354ff
Janis <janis@jaggedverge.com>
2018-04-24 15:39:36 +1000

    Cap maximum file upload size to avoid potential DOS attack

--- transcription_API_server.py
+++ transcription_API_server.py
@@ -1,6 +1,8 @@
 import connexion
 from connexion.resolver import RestyResolver
 
+from flask_uploads import patch_request_class
+
 import api
 
 app = connexion.FlaskApp(__name__, specification_dir='swagger/')
@@ -13,6 +15,9 @@ def index():
 <a href="/{version}">/{version}</a>. The API explorer tool can be found at
 <a href="/{version}/ui/">/{version}/ui/</a>, this is the best place to explore the API.
 """.format(version="v0.1")
+
+flask_app = app.app
+flask_app.config['MAX_CONTENT_LENGTH'] = 32 * 1024 * 1024 #max 32 MB file upload
 
 app.run(port=8080)
 
