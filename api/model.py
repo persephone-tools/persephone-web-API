b38eb4130d481385103a23241fd2c214413b72e9
Janis <janis@jaggedverge.com>
2018-04-25 03:00:16 +1000

    Create code entry point for listing models

---
+++ api/model.py
@@ -0,0 +1,8 @@
+"""
+API endpoints for /model
+This deals with the API access for model definitions and metadata
+"""
+
+def search():
+    print("Request for all available models")
+    return "Get available models not implemented", 501
