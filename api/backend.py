e8a93266a2f1a48478abb882c8540ddd800a567d
Janis <janis@jaggedverge.com>
2018-04-25 15:05:06 +1000

    Implement basic backend informational endpoint

--- api/backend.py
+++ api/backend.py
@@ -1,4 +1,15 @@
+"""
+API endpoints for /backend
+Get information about the backend capabilities supported through this API.
+"""
+import persephone
 
 def search():
-    print("Request for backend information")
-    return "Get backend information not implemented", 501
+    """
+    Handle a get request for the backend information
+    """
+    persephone_information = {
+        "name": "Persephone",
+        "version": persephone.__version__,
+    }
+    return persephone_information, 200
