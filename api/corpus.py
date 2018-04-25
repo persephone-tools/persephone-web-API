37827c8f86b5f2d6ca943b14109bf6a7e1eeda4c
Janis Lesinskis <janis@customprogrammingsolutions.com>
2018-04-25 19:15:51 +1000

    stub out endpoint for corpus API

---
+++ api/corpus.py
@@ -0,0 +1,8 @@
+"""
+API endpoints for /corpus
+This deals with the API access corpus model definitions and metadata
+"""
+
+def search():
+    print("Request for all available corpus")
+    return "Get available corpus not implemented", 501
