ebc453ebaaf9f47d4ab64eedb2c5287b298e1979
Janis <janis@jaggedverge.com>
2018-04-27 01:42:55 +1000

    Change upload path for development

--- api/upload_config.py
+++ api/upload_config.py
@@ -1,7 +1,9 @@
 import flask_uploads
+import os
 
 audio_files = flask_uploads.UploadSet(name="audio", extensions=("wav", "wave", "mp3"))
+base_upload_path = os.path.join(os.cwd(), 'user_uploads')
 
 def configure_uploads(flask_app):
-    flask_app.config['UPLOADED_AUDIO_DEST'] = '/var/uploads'
+    flask_app.config['UPLOADED_AUDIO_DEST'] = os.path.join(base_upload_path, 'audio_uploads')
     flask_uploads.configure_uploads(flask_app, (audio_files,))
