523539bc9b559963f88586f4ebffade8b7af3efa
Janis <janis@jaggedverge.com>
2018-04-26 21:26:47 +1000

    Create endpoint for corpus creation from Zip file

--- api/corpus.py
+++ api/corpus.py
@@ -6,3 +6,7 @@ This deals with the API access corpus model definitions and metadata
 def search():
     print("Request for all available corpus")
     return "Get available corpus not implemented", 501
+
+def create_from_zip():
+    print("Create corpus from zip file")
+    return "Create corpus from zip not implemented", 501
