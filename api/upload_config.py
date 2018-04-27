3309ee03da573a278bf21b49c63221fa69d4a411
Janis Lesinskis <janis@customprogrammingsolutions.com>
2018-04-27 18:40:32 +1000

    Fix function name

--- api/upload_config.py
+++ api/upload_config.py
@@ -2,7 +2,7 @@ import flask_uploads
 import os
 
 audio_files = flask_uploads.UploadSet(name="audio", extensions=("wav", "wave", "mp3"))
-base_upload_path = os.path.join(os.cwd(), 'user_uploads')
+base_upload_path = os.path.join(os.getcwd(), 'user_uploads')
 
 def configure_uploads(flask_app):
     flask_app.config['UPLOADED_AUDIO_DEST'] = os.path.join(base_upload_path, 'audio_uploads')
