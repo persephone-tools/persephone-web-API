114e74f218b872dba3e00743026f2e3f78ffd2ab
Janis Lesinskis <janis@customprogrammingsolutions.com>
2018-04-27 21:12:21 +1000

    Create upload path for text files

--- api/upload_config.py
+++ api/upload_config.py
@@ -2,8 +2,10 @@ import flask_uploads
 import os
 
 audio_files = flask_uploads.UploadSet(name="audio", extensions=("wav", "wave", "mp3"))
+text_files =  flask_uploads.UploadSet(name="text", extensions=("txt"))
 base_upload_path = os.path.join(os.getcwd(), 'user_uploads')
 
 def configure_uploads(flask_app):
     flask_app.config['UPLOADED_AUDIO_DEST'] = os.path.join(base_upload_path, 'audio_uploads')
-    flask_uploads.configure_uploads(flask_app, (audio_files,))
+    flask_app.config['UPLOADED_TEXT_DEST'] = os.path.join(base_upload_path, 'text_uploads')
+    flask_uploads.configure_uploads(flask_app, (audio_files, text_files))
