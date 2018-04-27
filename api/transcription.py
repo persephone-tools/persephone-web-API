67fbd059ff89d13413c0c17fcd2532890fa6dc0c
Janis Lesinskis <janis@customprogrammingsolutions.com>
2018-04-27 19:31:18 +1000

    Create ORM definition for transcriptions

--- api/transcription.py
+++ api/transcription.py
@@ -2,6 +2,20 @@
 API endpoints for /transcription
 This deals with the API access for transcription files uploading/downloading.
 """
+from sqlalchemy import Column, Integer, String
+from .orm_base import Base
+
+class Transcription(Base):
+    """Database ORM definition for Transcription files"""
+    __tablename__ = 'transcription'
+
+    id = Column(Integer, primary_key=True)
+    filename = Column(String)
+    url = Column(String)
+
+    def __repr__(self):
+        return "<Transcription(filename={}, url={})>".format(self.filename, self.url)
+
 
 def post(transcriptionFile):
     """handle POST request for transcription file"""
