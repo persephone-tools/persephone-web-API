ea271a56bd599d493f26b4fe39f66d12c5f6a82b
Janis Lesinskis <janis@customprogrammingsolutions.com>
2018-04-28 22:12:38 +1000

    Specify SQL connection in config

--- transcription_API_server.py
+++ transcription_API_server.py
@@ -2,19 +2,18 @@ import connexion
 from connexion.resolver import RestyResolver
 
 from flask_uploads import patch_request_class
-from sqlalchemy import create_engine
 
 from api.upload_config import configure_uploads
 
 import api
 
 #in-memory sqlite DB for development purposes, will need file backing for persistence
-engine = create_engine('sqlite:///:memory:', echo=True)
+#engine = create_engine('sqlite:///:memory:', echo=True)
 
+
+# Create the API endpoints from YAML specification
 app = connexion.FlaskApp(__name__, specification_dir='swagger/')
 app.add_api('api_spec.yaml', resolver=RestyResolver('api'))
-
-
 @app.route('/')
 def index():
     return """Access to the API is via the API versioned path prefix
@@ -23,6 +22,9 @@ def index():
 """.format(version="v0.1")
 
 flask_app = app.app
+
+flask_app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+
 flask_app.config['MAX_CONTENT_LENGTH'] = 64 * 1024 * 1024 #max 64 MB file upload
 configure_uploads(flask_app)
 
