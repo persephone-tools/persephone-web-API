d3335401afd7422d02aa25ca98bae58c40f62bb1
Janis <janis@jaggedverge.com>
2018-05-01 03:33:58 +1000

    Create DB model for utterances

--- api/db_models.py
+++ api/db_models.py
@@ -23,3 +23,19 @@ class Transcription(db.Model):
     def __repr__(self):
         return "<Transcription(filename={}, url={})>".format(self.filename, self.url)
 
+
+class Utterance(db.Model):
+    """Database ORM definiton for Utterances.
+    This consists of a relationship between an Audio file and a transcription file
+    """
+    __tablename__ = 'utterance'
+
+    id = db.Column(db.Integer, primary_key=True)
+    audio_id = db.Column(db.Integer, db.ForeignKey(Audio.id), primary_key=True)
+    transcription_id = db.Column(db.Integer, db.ForeignKey(Transcription.id), primary_key=True)
+
+    audio = db.relationship('Audio', foreign_keys='Audio.id')
+    transcription = db.relationship('Transcription', foreign_keys='Transcription.id')
+
+    def __repr__(self):
+        return "<Utterance(audio={}, transcription={})>".format(self.audio, self.transcription)
