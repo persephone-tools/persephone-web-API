d03c9b6d8c03ca14641561d951ad579004dadcca
Janis <janis@jaggedverge.com>
2018-05-07 00:54:35 +1000

    Use SQL to enforce integrity of utterance data

--- api/utterance.py
+++ api/utterance.py
@@ -1,26 +1,26 @@
 """
 API endpoints for /utterance
 """
-from .db_models import Utterance
+import sqlalchemy
 
 from . import db
-
+from .db_models import Utterance
 from .serialization import AudioSchema, TranscriptionSchema
 
 def post(utteranceInfo):
     """POST request"""
     audioId = utteranceInfo['audioId']
     transcriptionId = utteranceInfo['transcriptionId']
-    print("Got audioId {} transcriptionId {}".format(audioId, transcriptionId))
-    # TODO check that ID's for audio and transcription exist
-
-    current_utterance = Utterance(audio_id=audioId, transcription_id=transcriptionId)
-    db.session.add(current_utterance)
-    db.session.commit()
-
-    result = {
-        "id" : current_utterance.id,
-        "audio" : AudioSchema().dump(current_utterance.audio).data,
-        "transcription" : TranscriptionSchema().dump(current_utterance.transcription).data,
-    }
-    return result, 201
+    try:
+        current_utterance = Utterance(audio_id=audioId, transcription_id=transcriptionId)
+        db.session.add(current_utterance)
+        db.session.commit()
+    except sqlalchemy.exc.IntegrityError:
+        return "Invalid ID provided", 400
+    else:
+        result = {
+            "id" : current_utterance.id,
+            "audio" : AudioSchema().dump(current_utterance.audio).data,
+            "transcription" : TranscriptionSchema().dump(current_utterance.transcription).data,
+        }
+        return result, 201
