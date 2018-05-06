a0a4892caa8aa3f362fe63ed21d00d21706f9dce
Janis <janis@jaggedverge.com>
2018-05-07 00:33:07 +1000

    Turn on FK constraint checking for SQLite

--- api/db_models.py
+++ api/db_models.py
@@ -1,5 +1,16 @@
 from . import db
 
+from sqlalchemy import event
+from sqlalchemy.engine import Engine
+from sqlite3 import Connection as SQLite3Connection
+
+@event.listens_for(Engine, "connect")
+def _set_sqlite_pragma(dbapi_connection, connection_record):
+    if isinstance(dbapi_connection, SQLite3Connection):
+        cursor = dbapi_connection.cursor()
+        cursor.execute("PRAGMA foreign_keys=ON;")
+        cursor.close()
+
 class Audio(db.Model):
     """Database ORM definition for Audio files"""
     __tablename__ = 'audio'
