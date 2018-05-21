4a6d1e885caa8fac39947f0042de745e171edf21
Janis <janis@jaggedverge.com>
2018-05-21 20:45:30 +1000

    Start specifying API for model endpoint

--- api/model.py
+++ api/model.py
@@ -6,3 +6,6 @@ This deals with the API access for model definitions and metadata
 def search():
     print("Request for all available models")
     return "Get available models not implemented", 501
+
+def post():
+    raise NotImplementedError
