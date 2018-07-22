f253a10bea96cc6e702afa91d0d9eb10eef220bf
Janis <janis@jaggedverge.com>
2018-07-22 22:09:20 +1000

    Clean up imports

--- swagger/flask_app.py
+++ swagger/flask_app.py
@@ -1,7 +1,6 @@
 """ Define the endpoints that this API can handle via the swagger/OpenAPI YAML file"""
 
 import connexion
-from connexion.resolver import RestyResolver
 
 # Create the API endpoints from YAML specification
 connexion_app = connexion.FlaskApp(__name__, specification_dir='.')
--- transcription_API_server.py
+++ transcription_API_server.py
@@ -8,6 +8,8 @@ from swagger.flask_app import connexion_app, app
 from api.upload_config import configure_uploads
 import api
 
+# resolve the API
+from connexion.resolver import RestyResolver
 connexion_app.add_api('api_spec.yaml', resolver=RestyResolver('api'))
 
 
@@ -18,36 +20,33 @@ def index():
 <a href="/{version}/ui/">/{version}/ui/</a>, this is the best place to explore the API.
 """.format(version="v0.1")
 
-# fetch underlying flask app from the connexion app
-flask_app = app.app
-
 # configure the DB
 # in-memory sqlite DB for development purposes, will need file backing for persistence
-flask_app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
-flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 from api import db
-db.init_app(flask_app)
+db.init_app(app)
 
 # create DB tables
-with flask_app.app_context():
+with app.app_context():
     db.create_all()
 
 # configure upload paths
-flask_app.config['MAX_CONTENT_LENGTH'] = 64 * 1024 * 1024 #max 64 MB file upload
-flask_app.config['BASE_UPLOAD_DIRECTORY'] = os.path.join(os.getcwd(), 'user_uploads')
-configure_uploads(flask_app)
+app.config['MAX_CONTENT_LENGTH'] = 64 * 1024 * 1024 #max 64 MB file upload
+app.config['BASE_UPLOAD_DIRECTORY'] = os.path.join(os.getcwd(), 'user_uploads')
+configure_uploads(app)
 
 # persephone paths
-flask_app.config['CORPUS_PATH'] = os.path.join(os.getcwd(), 'persephone_corpus')
+app.config['CORPUS_PATH'] = os.path.join(os.getcwd(), 'persephone_corpus')
 
 
-@flask_app.route('/uploads/<path:path>')
+@app.route('/uploads/<path:path>')
 def uploaded_file(path):
     """Serve uploaded files for development purposes
     Note this is for development only, serve these files with Apache/nginx in production environments.
     """
-    return send_from_directory(flask_app.config['BASE_UPLOAD_DIRECTORY'],
+    return send_from_directory(app.config['BASE_UPLOAD_DIRECTORY'],
                                path)
 
 app.run(port=8080)
