4c9ff4e0e75fb7852f4a12dbc396a1eb2866df7d
Janis <janis@customprogrammingsolutions.com>
2018-07-28 22:01:02 +1000

    Add test for corpus construction via API

--- tests/test_corpus_endpoint.py
+++ tests/test_corpus_endpoint.py
@@ -1,4 +1,4 @@
-def test_corpus_creation(upload_audio, upload_transcription):
+def test_corpus_creation(client, upload_audio, upload_transcription):
     """Test that we are able to create a corpus object via the API"""
     import json
 
@@ -33,3 +33,68 @@ def test_corpus_creation(upload_audio, upload_transcription):
     assert response.status_code == 201
     transcription_response_data = json.loads(response.data.decode('utf8'))
     transcription_id_c = transcription_response_data['id']
+
+    data = {
+        "audioId": wav_id_a,
+        "transcriptionId": transcription_id_a
+    }
+
+    response = client.post(
+        '/v0.1/utterance',
+        data=json.dumps(data),
+        headers={'Content-Type': 'application/json'}
+    )
+    assert response.status_code == 201
+    utterance_response_data = json.loads(response.data.decode('utf8'))
+    utterance_id_a = utterance_response_data['id']
+
+    data = {
+        "audioId": wav_id_b,
+        "transcriptionId": transcription_id_b
+    }
+
+    response = client.post(
+        '/v0.1/utterance',
+        data=json.dumps(data),
+        headers={'Content-Type': 'application/json'}
+    )
+    assert response.status_code == 201
+    utterance_response_data = json.loads(response.data.decode('utf8'))
+    utterance_id_b = utterance_response_data['id']
+
+    data = {
+        "audioId": wav_id_c,
+        "transcriptionId": transcription_id_c
+    }
+
+    response = client.post(
+        '/v0.1/utterance',
+        data=json.dumps(data),
+        headers={'Content-Type': 'application/json'}
+    )
+    assert response.status_code == 201
+    utterance_response_data = json.loads(response.data.decode('utf8'))
+    utterance_id_c = utterance_response_data['id']
+
+    data = {
+        "name": "Test Corpus",
+        "label_type": "phonemes",
+        "feature_type": "fbank",
+        "testing": [
+            utterance_id_a
+        ],
+        "training": [
+            utterance_id_b
+        ],
+        "validation": [
+            utterance_id_c
+        ]
+    }
+
+    response = client.post(
+        '/v0.1/corpus',
+        data=json.dumps(data),
+        headers={'Content-Type': 'application/json'}
+    )
+
+    assert response.status_code == 201
