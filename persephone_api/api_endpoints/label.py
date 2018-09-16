881afef9fa86287a79a953ddc16341638dca63ba
Janis <janis@customprogrammingsolutions.com>
2018-09-16 17:13:14 +1000

    Implement label post endpoint

--- persephone_api/api_endpoints/label.py
+++ persephone_api/api_endpoints/label.py
@@ -1,10 +1,22 @@
 """Handlers for phonetic label functionality"""
 
+from ..extensions import db
 from ..db_models import Label
+from ..serialization import LabelSchema
 
 def search():
     """Handle request for all available labels"""
     raise NotImplementedError
 
 def post(labelInfo):
-    raise NotImplementedError
+    """Create a new phonetic label"""
+
+    current_label = Label(
+        label=labelInfo['phoneticLabel']
+    )
+
+    db.session.add(current_label)
+    db.session.commit()
+
+    result = LabelSchema().dump(current_label).data
+    return result, 201
