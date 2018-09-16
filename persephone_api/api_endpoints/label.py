063138df71b511015d4fa6f065ee0bf1e2e8eb75
Janis <janis@customprogrammingsolutions.com>
2018-09-16 16:52:08 +1000

    Stub out label endpoints

---
+++ persephone_api/api_endpoints/label.py
@@ -0,0 +1,10 @@
+"""Handlers for phonetic label functionality"""
+
+from ..db_models import Label
+
+def search():
+    """Handle request for all available labels"""
+    raise NotImplementedError
+
+def post(labelInfo):
+    raise NotImplementedError
