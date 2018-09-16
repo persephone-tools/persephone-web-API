13a3017c7c6e2a34e54cdf1fd268872fa26b5a3a
Janis <janis@customprogrammingsolutions.com>
2018-09-16 19:18:32 +1000

    Test endpoint for labels search

--- tests/test_labels.py
+++ tests/test_labels.py
@@ -15,3 +15,35 @@ def test_label_creation(client):
 
     assert response
     assert response.status_code == 201
+
+def test_label_listing(client):
+    """Test that we can create some labels then list all of the available ones"""
+    import json
+    data = {
+        "phoneticLabel": "a",
+    }
+
+    response = client.post(
+        '/v0.1/label',
+        data=json.dumps(data),
+        headers={'Content-Type': 'application/json'}
+    )
+
+    assert response.status_code == 201
+
+    data = {
+        "phoneticLabel": "b",
+    }
+
+    response = client.post(
+        '/v0.1/label',
+        data=json.dumps(data),
+        headers={'Content-Type': 'application/json'}
+    )
+    assert response.status_code == 201
+
+    response = client.get(
+        '/v0.1/label',
+    )
+
+    assert response
