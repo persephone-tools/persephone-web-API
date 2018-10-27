b4f534a07a03fc7271f0d53dea344e756f277e48
Janis <janis@customprogrammingsolutions.com>
2018-10-27 17:56:04 +1100

    Create serialization for file metadata

--- persephone_api/serialization.py
+++ persephone_api/serialization.py
@@ -3,8 +3,14 @@ Serialization for data defined in ORM/DB
 """
 
 from marshmallow_sqlalchemy import ModelSchema
+from marshmallow import fields
 
 from . import db_models
+
+class FileInfoSchema(ModelSchema):
+    """Serialization for file information"""
+    class Meta:
+        model = db_models.FileMetaData
 
 class AudioSchema(ModelSchema):
     class Meta:
@@ -12,6 +18,7 @@ class AudioSchema(ModelSchema):
         exclude = ("utterances",)
 
 class TranscriptionSchema(ModelSchema):
+    file_info = fields.Nested("FileInfoSchema")
     class Meta:
         model = db_models.Transcription
         exclude = ("utterances",)
