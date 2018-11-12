ab66152ac95c33b32ed1b93a2650e925f1059ccb
Janis Lesinskis <janis@customprogrammingsolutions.com>
2018-11-12 13:50:36 +1100

    Added test of label information endpoint

--- tests/test_API_backend.py
+++ tests/test_API_backend.py
@@ -2,3 +2,8 @@ def test_backend(client):
     """Test information about the transcriber library on the backend is provided"""
     response = client.get('/v0.1/backend')
     assert response.status_code == 200
+
+def test_label_support(client):
+    """Test endpoint that supplies information about supported label types."""
+    response = client.get('/v0.1/backend/labelTypes')
+    assert response.status_code == 200
