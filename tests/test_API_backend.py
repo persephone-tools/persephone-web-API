354fe14f686841de4569b2beee35d50fade1c1fe
Janis <janis@customprogrammingsolutions.com>
2018-11-13 18:28:39 +1100

    Added test for API endpoint for accepted files

--- tests/test_API_backend.py
+++ tests/test_API_backend.py
@@ -12,3 +12,15 @@ def test_feature_support(client):
     """Test endpoint that supplies information about supported feature types."""
     response = client.get('/v0.1/backend/featureTypes')
     assert response.status_code == 200
+
+def test_accepted_filetypes(client):
+    """Test the accepted file types endpoint"""
+    import json
+
+    response = client.get('/v0.1/backend/acceptedFiles')
+    assert response.status_code == 200
+
+    accepted_files_response_data = json.loads(response.data.decode('utf8'))
+    assert "audio" in accepted_files_response_data
+    assert "transcription" in accepted_files_response_data
+    assert "wav" in accepted_files_response_data["audio"]
