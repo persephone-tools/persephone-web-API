b9217aad10cf697e1cea44f35101e80518171229
Janis <janis@customprogrammingsolutions.com>
2018-11-17 17:24:58 +1100

    Add in type response default and fix parameters

--- persephone_api/error_response.py
+++ persephone_api/error_response.py
@@ -1,17 +1,17 @@
-"""RCF 7807 error handling"""
+"""Error response helpers for RFC 7807 error handling"""
 
 from typing import Tuple
 
-def error_information(error_type: str, title: str, status: str, detail: str, instance: str, **kwargs) -> Tuple[dict, int]:
+def error_information(*, title: str, status: int, detail: str, **kwargs) -> Tuple[dict, int]:
     """Construct an error response in the RFC 8707 format
     see https://tools.ietf.org/html/rfc7807
     """
     error_data = kwargs
     error_data.update({
-        "type": error_type,
         "title": title,
         "status": status,
         "detail": detail,
-        "instance": instance,
     })
+    if "type" not in kwargs:
+        error_data["type"] = "about:blank"
     return error_data, status
