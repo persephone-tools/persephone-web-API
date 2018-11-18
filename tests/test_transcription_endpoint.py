b7232c2536dd5e9782778f5eb8227aad3f1852d9
Janis <janis@customprogrammingsolutions.com>
2018-11-18 19:24:37 +1100

    Create simple test for transcription search endpoint

--- tests/test_transcription_endpoint.py
+++ tests/test_transcription_endpoint.py
@@ -49,6 +49,26 @@ def test_transcription_uploads_endpoint(client):
     transcription_response_data = json.loads(response.data.decode('utf8'))
     assert transcription_response_data['id']
 
+def test_search_endpoint(client):
+    import io
+    phonemes = "ɖ ɯ ɕ i k v̩"
+    data = {'transcriptionFile': (io.BytesIO(phonemes.encode('utf-8')), 'test_transcription_file.phonemes')}
+    response = client.post(
+        ('/v0.1/transcription/fromFile'),
+        data=data,
+        content_type='multipart/form-data'
+    )
+    assert response.status_code == 201
+
+    import json
+    transcription_response_data = json.loads(response.data.decode('utf8'))
+    assert transcription_response_data['id']
+    transcription_id = transcription_response_data['id']
+    response = client.get(
+        '/v0.1/transcription/',
+    )
+    assert response.status_code == 200
+    response_data = json.loads(response.data.decode('utf8'))
 
 def test_transcription_pagination(client):
     """Test that we can get pagination of results"""
