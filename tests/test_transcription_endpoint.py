68fda111177accb6da28be05f5aadd7161b137fd
Janis <janis@customprogrammingsolutions.com>
2018-11-18 19:08:09 +1100

    Create test for pagination

--- tests/test_transcription_endpoint.py
+++ tests/test_transcription_endpoint.py
@@ -48,3 +48,37 @@ def test_transcription_uploads_endpoint(client):
     import json
     transcription_response_data = json.loads(response.data.decode('utf8'))
     assert transcription_response_data['id']
+
+
+def test_transcription_pagination(client):
+    """Test that we can get pagination of results"""
+    import io
+    for t in range(10):
+        phonemes = "test "+ str(t)
+        filename = 'test_transcription_file{}.phonemes'.format(t)
+        data = {'transcriptionFile': (io.BytesIO(phonemes.encode('utf-8')), filename)}
+        response = client.post(
+            ('/v0.1/transcription/fromFile'),
+            data=data,
+            content_type='multipart/form-data'
+        )
+        assert response.status_code == 201
+
+    payload = {"pageSize": 6, "pageNumber": 1}
+    response = client.get(
+        '/v0.1/transcription/',
+        data=payload,
+    )
+    assert response.status_code == 200
+    import json
+    response_data = json.loads(response.data.decode('utf8'))
+    assert len(response_data) == 6
+
+    payload = {"pageSize": 6, "pageNumber": 2}
+    response = client.get(
+        '/v0.1/transcription/',
+        params=payload,
+    )
+    assert response.status_code == 200
+    response_data = json.loads(response.data.decode('utf8'))
+    assert len(response_data) == 4 # only 4 items remaining
