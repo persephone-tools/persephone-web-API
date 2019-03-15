03bdace7c02c23d7f254de730fc295f8aff9bd82
Janis Lesinskis <janis@customprogrammingsolutions.com>
2019-03-15 18:45:06 +1100

    Fix endpoint function name

--- persephone_api/api_endpoints/bulk_data.py
+++ persephone_api/api_endpoints/bulk_data.py
@@ -5,6 +5,9 @@ Make these endpoints be behind an auth wall if possible.
 
 from ..db_models import FileMetaData
 
-def utterance(utterancesFile):
+from ..upload_config import compressed_files, uploads_url_base
+
+
+def utterances(utterancesFile):
     """handle POST request for bulk utterances file uploading"""
     raise NotImplementedError("Bulk upload not implemented yet")
