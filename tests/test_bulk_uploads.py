9d967a8922c7ee4d60809f9ff5ecad5ad336deed
Janis Lesinskis <janis@customprogrammingsolutions.com>
2019-03-15 18:45:26 +1100

    Added a failing test for bulk upload endpoint

---
+++ tests/test_bulk_uploads.py
@@ -0,0 +1,15 @@
+def test_invalid_file_upload(init_database, client):
+    """Test that accidentally uploading a text file (such as a phonemes file)
+    triggers an error, this endpoint needs a compressed file type uploaded to it."""
+    import io
+    data = {'utterancesFile': (io.BytesIO(b"this is not a compressed file"), 'accidental.phonemes')}
+    response = client.post(
+        ('/v0.1/bulk_data/utterances'),
+        data=data,
+        content_type='multipart/form-data'
+    )
+    assert response.status_code == 415
+
+    import json
+    wav_response_data = json.loads(response.data.decode('utf8'))
+    assert wav_response_data['status'] == 415
