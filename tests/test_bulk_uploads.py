80cb578696567405241b26c8824ce6a80c290bc8
Janis Lesinskis <janis@customprogrammingsolutions.com>
2019-03-15 19:00:29 +1100

    Added failing test for bulk upload endpoint taking an empty zip file

--- tests/test_bulk_uploads.py
+++ tests/test_bulk_uploads.py
@@ -13,3 +13,20 @@ def test_invalid_file_upload(init_database, client):
     import json
     upload_response_data = json.loads(response.data.decode('utf8'))
     assert upload_response_data['status'] == 415
+
+
+def test_empty_zip_file_fails(init_database, client):
+    """Test that a zip file with no contents does not pass validation"""
+    import io
+    empty_zip_file = b'PK\x05\x06\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
+    data = {'utterancesFile': (io.BytesIO(empty_zip_file), 'empty.zip')}
+    response = client.post(
+        ('/v0.1/bulk_data/utterances'),
+        data=data,
+        content_type='multipart/form-data'
+    )
+    assert response.status_code == 400
+
+    import json
+    upload_response_data = json.loads(response.data.decode('utf8'))
+    assert upload_response_data['status'] == 400
